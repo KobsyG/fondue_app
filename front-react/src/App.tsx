9e2a82a1b2fcfffe6b82f67ee825758740121450
JBVer <jeanbaptistevermeersch@gmail.com>
2023-10-30 19:41:15 +0100

    première frite parallax ayant jamais existé

--- front-react/src/App.tsx
+++ front-react/src/App.tsx
@@ -1,52 +1,41 @@
-import React from 'react';
 import './App.css';
-// import Header from './components/Header/Header';
-// import Footer from './components/Footer/Footer';
-// import FirstImg from './components/FirstImg/FirstImg';
-import Cascade from './components/FirstImg/cascade.png'
-import Space from './components/FirstImg/banner-background.jpg'
-import { Parallax, ParallaxBanner } from 'react-scroll-parallax';
-import { useParallax } from 'react-scroll-parallax';
-import { BannerLayer } from 'react-scroll-parallax/dist/components/ParallaxBanner/types';
+import { Parallax, } from 'react-scroll-parallax';
+import { useState, useRef, useEffect } from 'react';
 
-
-const BG = () => {
-  const background: BannerLayer = {
-    children: (
-      // <div className='absolute h-96 w-full bg-black'/>
-      <div className="absolute h-96 inset-0 bg-gradient-to-t from-gray-900 to-blue-900" />
-    ),
-    // image:'./components/FirstImg/banner-background.jpg',
-    translateY: [50, 200],
-    // translateY: [0, 50],
-    opacity: [1, 0.3],
-    scale: [1.05, 1, 'easeOutCubic'],
-    // shouldAlwaysCompleteAnimation: true,
-  };
+const Backg = () => {
   return (
-    <ParallaxBanner
-      layers={[background]}
-      className='aspect-[2/1]'
-    />
-  )
-}
-
-const Squre = () => {
-  return (
-    // <Parallax startScroll={50} endScroll={100}>
-    <Parallax startScroll={50} endScroll={250} translateY={[-100, 100]}>
-      <div className='h-48 w-48 bg-white' />
-    </Parallax>
+    <div className="absolute w-full h-full z-0">
+      <div className='w-full h-96 bg-gray-400' />
+      <div className='w-full h-96 bg-gray-100' />
+      <div className='w-full h-96 bg-gray-400' />
+      <div className='w-full h-96 bg-gray-100' />
+      <div className='w-full h-96 bg-gray-400' />
+      <div className='w-full h-96 bg-gray-100' />
+      <div className='w-full h-96 bg-gray-400' />
+      <div className='w-full h-96 bg-gray-100' />
+    </div>
   )
 }
 
 function App() {
+  const [targetElement, setTarget] = useState<HTMLDivElement | null>(null);
+  const targetRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    setTarget(targetRef.current);
+  }, [targetRef]);
   return (
-    <div className='w-full h-large-page bg-fondue-yellow'>
-      {/* <BG /> */}
-      <Squre/>   
-    </div >
-  );
+    <div className='w-full h-large-page bg-fondue-yellow justify-center items-center'>
+      <Backg />
+      <div ref={targetRef} className="absolute top-0 left-1/2 mt-[100vh] h-[900px] w-20 bg-fondue-red" />
+      {targetElement && (
+        <Parallax
+          translateY={['100vh', '250vh']}
+          targetElement={targetElement}
+          className='mt-50 ml-12 h-48 w-48 bg-fondue-blue'
+        />
+      )}
+    </div>
+  )
 }
 
 export default App;

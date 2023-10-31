73807a65d7e34e58ded927e4c7f543abeba035ae
JBVer <jeanbaptistevermeersch@gmail.com>
2023-10-31 22:41:38 +0100

    integrate Locomotive scroll JS lib within App.tsx

--- front-react/src/App.tsx
+++ front-react/src/App.tsx
@@ -1,12 +1,40 @@
-import { Parallax, useParallax } from 'react-scroll-parallax';
 import './App.css';
 import { useEffect, useRef, useState } from 'react';
-import { RootMarginShape } from 'parallax-controller';
-import ProtoMenInBlack from './components/ProtoMenInBLack/ProtoMenInBlack';
+import LocomotiveScroll from 'locomotive-scroll';
+
+function LocoLoco() {
+  return (
+    <div className='h-large-page'>
+      <div className='h-[50vh]' />
+      <h1
+        data-scroll data-scroll-speed="3"
+        data-scroll-position="top">
+        Locomotive Scroll in React
+      </h1>
+      <h2
+        data-scroll
+        data-scroll-speed="2"
+        data-scroll-position="top"
+        data-scroll-direction="horizontal"
+      >
+        Ima go sideways
+      </h2>
+    </div>
+  )
+}
 
 function App() {
+  const scrollContainerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: scrollContainerRef.current ? scrollContainerRef.current : undefined,
+      smooth: true
+    });
+  }, [scrollContainerRef]);
   return (
-    <div>
+    <div ref={scrollContainerRef}>
+      <LocoLoco />
     </div>
   )
 }
--- front-react/src/index.tsx
+++ front-react/src/index.tsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ParallaxProvider } from 'react-scroll-parallax';
+// import { ParallaxProvider } from 'react-scroll-parallax';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ParallaxProvider>
-      <App />
-    </ParallaxProvider>
+    {/* <ParallaxProvider> */}
+    <App />
+    {/* </ParallaxProvider> */}
   </React.StrictMode>
 );
 

f6c8da93916a6901a4f582dce64ca7785ee41847
KobeLaC <g.beco015@gmail.com>
2023-11-05 09:26:16 +0100

    ~

--- front-react/src/components/FirstImg/FirstImg.tsx
+++ front-react/src/components/FirstImg/FirstImg.tsx
@@ -1,21 +1,46 @@
-import React from 'react'
-import { ParallaxBanner } from 'react-scroll-parallax'
+import React, { useEffect, useRef } from 'react'
 import vague from './vague.png'
+import {gsap, Power2} from 'gsap'
 
 const FirstImg = () => {
+  
+  let container = useRef(null)
+  let image = useRef(null)
+  let rideau = useRef(null)
+  var tl = gsap.timeline();
+
+  useEffect(() => {
+    tl.to(
+    container.current , {visibility: "visible" }
+    ).to(
+    rideau.current, {x: '100vh', ease: Power2.easeInOut},
+    ).from(
+      image.current, {scale: 1.6, ease: Power2.easeInOut, delay: -1.4},
+    );
+
+    // gsap.to(
+    //   container, {
+    //   ScrollTrigger: {
+    //     trigger: 
+    //     scrub: true,
+    //   }
+
+      
+    
+
+  });
+  
   return (
-    // <div>
-        // <img src={require("./image.png")} alt="firstimg" />
-        <ParallaxBanner
-          layers={[
-          // {image: vague, speed: -10},
-          {image: vague, translateY: [-20, 50]},
-        ]}
-        className='aspect-[2/1]'
-        >
-          {/* <h1>Hello</h1> */}
-          </ParallaxBanner>
-    //{/* </div> */}
+    <div className='container'>
+      <div
+        ref={container}
+        className='img-container'>
+        <img
+        ref={image}
+        src={require("./photo-test.webp")} alt="firstimg"/>
+        <div ref={rideau} className='rideau'></div>
+      </div>
+    </div>
   )
 }
 

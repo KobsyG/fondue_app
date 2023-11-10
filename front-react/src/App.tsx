2358efee26ed4f014b14708287bf049c2089501a
KobeLaC <g.beco015@gmail.com>
2023-11-10 15:17:13 +0100

    mv files for fresh start

--- front-react/src/App.tsx
+++ front-react/src/App.tsx
@@ -3,19 +3,18 @@ import './App.css';
 import TopImageMaskDown from './components/JB/TopImageMaskDown';
 import FallingCheese from './components/JB/FallingCheese';
 import MaskUpScaleDown from './components/JB/MaskUpScaleDown';
-import FirstPart from './components/FirstPart/FirstPart';
+import FirstPart from './components/Kobe/FirstPart/FirstPart';
 import LaBelleHistoire from './components/JB/LaBelleHistoire';
 import CloudCheese from './components/JB/CloudCheese';
-import Terroir from './components/Terroir/Terroir';
+import Terroir from './components/Kobe/Terroir/Terroir';
+import Actuel from './components/Kobe/Actuel/Actuel';
 
 function App() {
 
   return (
       <ReactLenis root>
 
-      <FirstPart />
-      <LaBelleHistoire />
-      <Terroir />
+      <Actuel />
 
       </ReactLenis>
   )
--- front-react/src/components/Kobe/Actuel/Actuel.tsx
+++ front-react/src/components/Kobe/Actuel/Actuel.tsx
@@ -2,24 +2,14 @@ import React, { useEffect, useRef } from 'react'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { gsap } from 'gsap'
 import './Actuel.css'
-import ImgZoom from '../../FirstPart/ImgZoom'
-import LoopAnim from '../../FirstPart/LoopAnim'
+import ImgZoom from '../FirstPart/ImgZoom'
+import LoopAnim from '../FirstPart/LoopAnim'
 
 const Actuel = () => {
-  
-    
 
- 
-
-    return (
+  return (
     <>
-    <div className='container-1st-part w-full h-[200vh] bg-orange-500 flex justify-center'>
-        
-        <ImgZoom />
-
-        
-    </div>
-    <div className='w-full h-[100vh] bg-orange-500' />
+      
     </>
   )
 }

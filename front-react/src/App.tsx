c2de1a9f356c1a4844c6f41dac2d6f0966a8c085
JBVer <jeanbaptistevermeersch@gmail.com>
2023-11-14 13:52:23 +0100

    nada

--- front-react/src/App.tsx
+++ front-react/src/App.tsx
@@ -1,32 +1,24 @@
-import { ReactLenis } from '@studio-freight/react-lenis'
-import './App.css';
-import TopImageMaskDown from './components/JB/TopImageMaskDown';
-import FallingCheese from './components/JB/FallingCheese';
-import MaskUpScaleDown from './components/JB/MaskUpScaleDown';
-import FirstPart from './components/Kobe/FirstPart/FirstPart';
-import LaBelleHistoire from './components/JB/LaBelleHistoire';
-import CloudCheese from './components/JB/CloudCheese';
-import Effect1 from './components/JB/Effect1';
 
-//fonts
-// import './fonts/AvocadoCake.otf';
-import Terroir from './components/Kobe/Terroir/Terroir';
+import React from 'react';
+import { ReactLenis } from '@studio-freight/react-lenis';
+import './App.css';
+
 import Actuel from './components/Kobe/Actuel/Actuel';
 
 function App() {
 
   return (
-      <ReactLenis root>
 
+    <ReactLenis root>
       {/* <MaskUpScaleDown /> */}
       {/* <TopImageMaskDown /> */}
       {/* <LaBelleHistoire /> */}
       {/* <Effect1 /> */}
       {/* <CloudCheese /> */}
       <Actuel />
+    </ReactLenis>
 
 
-      </ReactLenis>
   )
 }
 

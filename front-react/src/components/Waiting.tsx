5f32ea2b33cef726b95de0893836dfea2f0c16d1
KobeLaC <g.beco015@gmail.com>
2023-11-22 12:26:56 +0100

    waiting WIP

--- front-react/src/components/Waiting.tsx
+++ front-react/src/components/Waiting.tsx
@@ -1,8 +1,8 @@
 import gsap from 'gsap'
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 
-const couleeTel = require("../images/new/Coulée_pc.png")
-const couleePc = require("../images/new/Coulée_pc.png")
+const couleeTel = require("../images/new/Coulee_pc_waiting.png")
+const couleePc = require("../images/new/Coulee_pc_waiting.png")
 const bigWave = require("../images/new/Grosse_vague_pc.png")
 const middleWave = require("../images/new/Moyenne_vague_pc.png")
 const smallWave = require("../images/new/Petite_vague_pc.png")
@@ -12,7 +12,7 @@ const goutte2 = require("../images/new/Goutte2pc.png")
 const goutte3 = require("../images/new/Goutte3pc.png")
 const goutte4 = require("../images/new/Goutte4pc.png")
 const goutte5 = require("../images/new/Goutte5pc.png")
-const goutte6 = require("../images/new/Goutte6pc.png")
+const logo = require("../images/logo-bleu.png")
 
 const Waiting = () => {
 
@@ -29,15 +29,41 @@ const Waiting = () => {
             gsap.to(".goutte-container", {
                 yPercent: 100,
                 ease: 'power3.in',
-                duration: 1,
+                duration: 10,
                 delay: 2,
                 repeat: -1,
-                repeatDelay: 6,
+                repeatDelay: 1,
             })
 
             const waveTl = gsap.timeline();
 
+            waveTl.fromTo('.big-wave',
+                {
+                    x: '8vw',
+                },
+                {
+                    x: '-8vw',
+                    yoyo: true,
+                    repeat: -1,
+                    duration: 3.5,
+                    ease: 'power1.inOut',
+                }, '0'
+            )
+
             waveTl.fromTo('.middle-wave',
+                {
+                    x: '5vw',
+                },
+                {
+                    x: '-5vw',
+                    yoyo: true,
+                    repeat: -1,
+                    duration: 3.5,
+                    ease: 'power1.inOut',
+                }, '0'
+            )
+
+            waveTl.fromTo('.small-wave',
                 {
                     x: '-10vw',
                 },
@@ -45,9 +71,9 @@ const Waiting = () => {
                     x: '10vw',
                     yoyo: true,
                     repeat: -1,
-                    duration: 3,
-                    ease: 'none',
-                }
+                    duration: 6,
+                    ease: 'power1.inOut',
+                }, '0'
             )
 
         })
@@ -60,62 +86,65 @@ const Waiting = () => {
     // }, [isClickedMenu])
 
     return (
-        <div className='waiting-container relative overflow-hidden w-full h-[100vh] bg-fondue-blue'>
+        <div className='waiting-container relative overflow-hidden w-[100vw] h-[100vh] bg-fondue-blue'>
+
+            <div className='goutte-container absolute top-0 h-[100vh] w-[100vw]'>
+                <div className='goutte1 absolute top-0'>
+                    <img src={goutte1} className='w-full object-cover' />
+                </div>
+                <div className='goutte2 absolute top-0'>
+                    <img src={goutte2} className='w-full object-cover' />
+                </div>
+                <div className='goutte3 absolute top-0'>
+                    <img src={goutte3} className='w-full object-cover' />
+                </div>
+                <div className='goutte4 absolute top-0'>
+                    <img src={goutte4} className='w-full object-cover' />
+                </div>
+                <div className='goutte5 absolute top-0'>
+                    <img src={goutte5} className='w-full object-cover' />
+                </div>
+
+            </div>
 
             <div className='coulee relative'>
                 <picture>
                     <source
                         media='(max-width: 1024px)'
                         srcSet={couleeTel}
+                        className='w-full h-full object-cover'
                     />
                     <source
                         media='(min-width: 1025px)'
                         srcSet={couleePc}
+                        className='w-full h-full object-cover'
                     />
-                    <img src={couleePc} />
+                    <img className='w-full h-full object-cover' src={couleePc} />
                 </picture>
 
             </div>
 
-            <div className='goutte-container absolute top-0 h-[100vh] w-[100vw]'>
-                <div className='goutte1'>
-                    <img src={goutte1} className='w-full' />
-                </div>
-                <div className='goutte2'>
-                    <img src={goutte2} className='w-full' />
-                </div>
-                <div className='goutte3'>
-                    <img src={goutte3} className='w-full' />
-                </div>
-                <div className='goutte4'>
-                    <img src={goutte4} className='w-full' />
-                </div>
-                <div className='goutte5'>
-                    <img src={goutte5} className='w-full' />
-                </div>
-                <div className='goutte6'>
-                    <img src={goutte6} className='w-full' />
-                </div>
-            </div>
 
-            <div className='text relative' >
-                <img src={texte} />
-            </div>
+
 
             <div className='waves-container absolute w-full bottom-0 flex justify-center' >
 
-                <div className='big-wave absolute w-[120vw] bottom-0' >
+                <div className='big-wave absolute w-[200vw] bottom-0' >
                     <img src={bigWave} className='' />
                 </div>
 
-                <div className='middle-wave absolute w-[120vw] bottom-0' >
+                <div className='middle-wave absolute w-[200vw] bottom-0' >
                     <img src={middleWave} className='' />
                 </div>
 
-                <div className='bottom-wave absolute w-[120vw] bottom-0' >
+                <div className='small-wave absolute w-[200vw] bottom-0' >
                     <img src={smallWave} className='' />
                 </div>
 
+            </div>
+
+            <div style={{ top: '60%', transform: 'translateY(-50%)' }} className='text absolute w-[90%] left-[5%] flex justify-center' >
+                <img src={texte} className='object-cover w-full' />
             </div>
 
             <button className='buy-button absolute top-2 right-20 h-10 w-10'>

f3e88c6e6365c814de4483ff37a9ae2ca4d23784
KobeLaC <g.beco015@gmail.com>
2023-11-22 19:38:08 +0100

    ~

--- front-react/src/components/Waiting.tsx
+++ front-react/src/components/Waiting.tsx
@@ -16,78 +16,78 @@ const logo = require("../images/logo-bleu.png")
 
 const Waiting = () => {
 
-  const [isClickedMenu, setIsClickedMenu] = useState(false);
+    const [isClickedMenu, setIsClickedMenu] = useState(false);
 
-  function handleMenuCLick() {
-    setIsClickedMenu(!isClickedMenu);
-  }
+    function handleMenuCLick() {
+        setIsClickedMenu(!isClickedMenu);
+    }
 
-  useLayoutEffect(() => {
+    useLayoutEffect(() => {
 
-    let ctx = gsap.context(() => {
+        let ctx = gsap.context(() => {
 
-      gsap.to(".goutte-container", {
-        yPercent: 100,
-        ease: 'power3.in',
-        duration: 10,
-        delay: 2,
-        repeat: -1,
-        repeatDelay: 1,
-      })
+            gsap.to(".goutte-container", {
+                yPercent: 100,
+                ease: 'power3.in',
+                duration: 10,
+                delay: 2,
+                repeat: -1,
+                repeatDelay: 1,
+            })
 
-      const waveTl = gsap.timeline();
+            const waveTl = gsap.timeline();
 
-      waveTl.fromTo('.big-wave',
-        {
-          x: '8vw',
-        },
-        {
-          x: '-8vw',
-          yoyo: true,
-          repeat: -1,
-          duration: 3.5,
-          ease: 'power1.inOut',
-        }, '0'
-      )
+            waveTl.fromTo('.big-wave',
+                {
+                    x: '8vw',
+                },
+                {
+                    x: '-8vw',
+                    yoyo: true,
+                    repeat: -1,
+                    duration: 3.5,
+                    ease: 'power1.inOut',
+                }, '0'
+            )
 
-      waveTl.fromTo('.middle-wave',
-        {
-          x: '5vw',
-        },
-        {
-          x: '-5vw',
-          yoyo: true,
-          repeat: -1,
-          duration: 3.5,
-          ease: 'power1.inOut',
-        }, '0'
-      )
+            waveTl.fromTo('.middle-wave',
+                {
+                    x: '5vw',
+                },
+                {
+                    x: '-5vw',
+                    yoyo: true,
+                    repeat: -1,
+                    duration: 3.5,
+                    ease: 'power1.inOut',
+                }, '0'
+            )
 
-      waveTl.fromTo('.small-wave',
-        {
-          x: '-10vw',
-        },
-        {
-          x: '10vw',
-          yoyo: true,
-          repeat: -1,
-          duration: 6,
-          ease: 'power1.inOut',
-        }, '0'
-      )
+            waveTl.fromTo('.small-wave',
+                {
+                    x: '-10vw',
+                },
+                {
+                    x: '10vw',
+                    yoyo: true,
+                    repeat: -1,
+                    duration: 6,
+                    ease: 'power1.inOut',
+                }, '0'
+            )
 
-    })
+        })
 
-    return () => ctx.revert();
-  }, [])
+        return () => ctx.revert();
+    }, [])
 
-  // useEffect(() => {
-  //     menuRef.current
-  // }, [isClickedMenu])
+    // useEffect(() => {
+    //     menuRef.current
+    // }, [isClickedMenu])
 
-  return (
-    <div className='waiting-container relative overflow-hidden bg-fondue-blue w-[100vw] h-[100vh]'>
-      {/*         
+    return (
+        <div className='waiting-container relative overflow-hidden bg-fondue-blue w-[100vw] h-[100vh]'>
+            {/*         
             <div className='goutte-container absolute top-0 h-[100vh] w-[100vw]'>
                 <div className='goutte1 absolute top-0'>
                     <img src={goutte1} className='w-full h-full object-cover' />
@@ -108,29 +108,29 @@ const Waiting = () => {
             </div>
             */}
 
-      <div className='bg-[#FFDA8F] absolute top-0 h-[30%] w-full p-6 flex justify-center'>
-        <div className='h-full aspect-[1/1] rounded-full bg-fondue-red p-1'>
-          <img src={logo} className='h-full' />
-        </div>
-      </div>
+            <div className='bg-[#FFDA8F] absolute top-0 h-[30%] w-full p-6 flex justify-center'>
+                <div className='h-full aspect-[1/1] rounded-full bg-fondue-red p-1'>
+                    <img src={logo} className='h-full' />
+                </div>
+            </div>
 
-      <div className='coulee absolute top-[30%]'>
-        <picture>
-          <source
-            media='(max-width: 1024px)'
-            srcSet={couleeTel}
-            className='w-full'
-          />
-          <source
-            media='(min-width: 1025px)'
-            srcSet={couleePc}
-            className='w-full'
-          />
-          <img className='w-full' src={couleePc} />
-        </picture>
-      </div>
+            <div className='coulee absolute top-[30%]'>
+                <picture>
+                    <source
+                        media='(max-width: 1024px)'
+                        srcSet={couleeTel}
+                        className='w-full'
+                    />
+                    <source
+                        media='(min-width: 1025px)'
+                        srcSet={couleePc}
+                        className='w-full'
+                    />
+                    <img className='w-full' src={couleePc} />
+                </picture>
+            </div>
 
-      {/* 
+            {/* 
       <div className='waves-container absolute top-[60%] w-full flex flex-col justify-center' >
         <div className='big-wave relative w-[200vw]' >
           <img src={bigWave} className='' />
@@ -147,25 +147,27 @@ const Waiting = () => {
         <div className='bg-cyan-200 relative h-[100vh] w-full' />
       </div> */}
 
-      <div className='absolute top-[60%] flex flex-col w-full'>
+            <div className='absolute h-fit w-full top-[60%] flex flex-col'>
 
-        <div className='waves-container relative w-full h-auto flex justify-center' >
-          <div className='big-wave absolute w-[200vw]' >
-            <img src={bigWave} className='' />
-            <div className='middle-wave absolute w-[200vw] bottom-0' >
-              <img src={middleWave} className='' />
+                <div className='waves-container relative w-full' >
+                    <div className='big-wave relative w-[200vw] bottom-0'>
+                        <img src={bigWave} className='' />
+                    </div>
+                    <div className='middle-wave absolute w-[200vw] bottom-0' >
+                        <img src={middleWave} className='' />
+                    </div>
+
+                    <div className='small-wave absolute w-[200vw] bottom-0' >
+                        <img src={smallWave} className='' />
+                    </div>
+
+                </div>
+
+                <div className='bg-cyan-200 h-[100vh] w-full' />
+
             </div>
 
-            <div className='small-wave absolute w-[200vw] bottom-0' >
-              <img src={smallWave} className='' />
-            </div>
-          </div>
-        </div>
-
-        <div className='bg-cyan-200 h-[100vh] w-full' />
-      </div>
-
-      {/* 
+            {/* 
       <div className='waves-container absolute w-full bottom-0 flex justify-center' >
         <div className='big-wave absolute w-[200vw] bottom-0' >
           <img src={bigWave} className='' />
@@ -180,37 +182,37 @@ const Waiting = () => {
         </div>
       </div> */}
 
-      <div style={{ top: '60%', transform: 'translateY(-50%)' }} className='text absolute w-[90%] left-[5%] flex justify-center' >
-        <img src={texte} className='object-cover w-full' />
-      </div>
+            <div style={{ top: '60%', transform: 'translateY(-50%)' }} className='text absolute w-[90%] left-[5%] flex justify-center' >
+                <img src={texte} className='object-cover w-full' />
+            </div>
 
-      <button className='buy-button absolute top-2 right-20 h-10 w-10'>
-        <img src={require('../images/new/Logo_panier.png')} />
-      </button>
+            <button className='buy-button absolute top-2 right-20 h-10 w-10'>
+                <img src={require('../images/new/Logo_panier.png')} />
+            </button>
 
-      <button className='menu-button absolute top-2 right-3 h-10 w-10' onClick={handleMenuCLick} >
-        <img src={require('../images/new/Logo_menu.png')} />
-      </button>
+            <button className='menu-button absolute top-2 right-3 h-10 w-10' onClick={handleMenuCLick} >
+                <img src={require('../images/new/Logo_menu.png')} />
+            </button>
 
-      {/* <div style={{visibility: isClickedMenu ? 'visible' : 'hidden',
+            {/* <div style={{visibility: isClickedMenu ? 'visible' : 'hidden',
             opacity: isClickedMenu ? 1 : 0,
             transition: '1s'}} className='menu absolute w-40 top-12 right-0 bg-red-500'> */}
-      {/* <div style={{height: isClickedMenu ? 80 : 0, transition: '2s'}} 
+            {/* <div style={{height: isClickedMenu ? 80 : 0, transition: '2s'}} 
             className='menu absolute w-40 top-12 right-0 overflow-hidden bg-red-500'> */}
-      <div style={{
-        clipPath: isClickedMenu ? 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)' : 'polygon(100% 0%, 100% 0%, 100% 0%, 100% 0%)',
-        transition: '2s'
-      }}
-        className='menu absolute w-40 top-12 right-0 overflow-hidden bg-red-500'>
-        <ul>
-          <li>Facebook</li>
-          <li>Instagram</li>
-          <li>Contact</li>
-        </ul>
-      </div>
+            <div style={{
+                clipPath: isClickedMenu ? 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)' : 'polygon(100% 0%, 100% 0%, 100% 0%, 100% 0%)',
+                transition: '2s'
+            }}
+                className='menu absolute w-40 top-12 right-0 overflow-hidden bg-red-500'>
+                <ul>
+                    <li>Facebook</li>
+                    <li>Instagram</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Waiting

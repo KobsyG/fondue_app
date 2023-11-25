aaf5af3a7b1b5f177d1e9bef22feb06af4fead45
KobeLaC <g.beco015@gmail.com>
2023-11-25 17:49:26 +0100

    pulled Beco

--- front-react/src/components/JB/ProtoMenInBLack/ProtoMenInBlack.tsx
+++ front-react/src/components/JB/ProtoMenInBLack/ProtoMenInBlack.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from "react";
-import { Parallax } from "react-scroll-parallax";
 import Portrait from '../../../images/jb/portrait_of_black_man.jpg'
 
 function ProtoMenInBlack() {
@@ -16,7 +15,7 @@ function ProtoMenInBlack() {
                 <div ref={targetRef} className="top-0 left-1/4 h-[100vh] w-10 bg-fondue-red" />
             </div>
             <div className=''>
-                {targetElement && (
+                {/* {targetElement && (
                     <Parallax
                         scale={[1, 0.30]}
                         translateY={['0vh', '12vh']}
@@ -26,7 +25,7 @@ function ProtoMenInBlack() {
                     >
                         <img className='object-cover w-full' src={Portrait} />
                     </Parallax>
-                )}
+                )} */}
             </div>
         </div >
     )
